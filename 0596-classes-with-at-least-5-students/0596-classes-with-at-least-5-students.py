afbcbe750a522dd583d3d1ebfbb7025d166a8397
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-07-06 23:46:05 -0400

    Time: 282 ms (38.49%), Space: 67.7 MB (92.2%) - LeetHub

---
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def find_classes(courses: pd.DataFrame) -> pd.DataFrame:
+    result=courses.groupby('class')['student'].count().reset_index()
+    return result[result['student']>=5][['class']]
+
+
