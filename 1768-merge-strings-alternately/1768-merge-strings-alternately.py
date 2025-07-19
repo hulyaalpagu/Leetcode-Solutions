453d0871ff797e975ce4fb81947506b6402164c4
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-07-18 23:31:19 -0400

    Time: 20 ms (29.57%), Space: 12.6 MB (12.87%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        """
+        :type word1: str
+        :type word2: str
+        :rtype: str
+        """
+        max_len=max(len(word1),len(word2))
+        result=[]
+
+        for i in range(max_len):
+            if i<len(word1):
+                result.append(word1[i])
+            if i<len(word2):
+                result.append(word2[i])
+            
+        return ''.join(result)
+
+     
+
+
+
+
+        
