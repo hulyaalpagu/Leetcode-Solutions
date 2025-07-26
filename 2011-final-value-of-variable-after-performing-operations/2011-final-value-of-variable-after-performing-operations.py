12b122c5be99bbb4ee5a1690e5d6bf7d0ee58ab6
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-07-25 23:19:29 -0400

    Time: 0 ms (100%), Space: 12.4 MB (81.81%) - LeetHub

---
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        """
+        :type operations: List[str]
+        :rtype: int
+        """
+        X = 0
+        for operation in operations:
+            if '++' in operation:
+                X += 1
+            else:
+                X -= 1
+        return X
