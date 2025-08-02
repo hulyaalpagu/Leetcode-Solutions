f21f3bfdfa277b62d3431cb4a42972e061d38efc
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-08-01 21:43:27 -0400

    Time: 0 ms (100%), Space: 12.5 MB (51.39%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if str(x) == str(x)[::-1] and x >= 0:
+            return True
+        else:
+            return False
