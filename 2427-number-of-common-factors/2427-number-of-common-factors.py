e78499472ca46ee93376c377434047b23f15ea6f
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-08-12 23:26:55 -0400

    Time: 0 ms (100%), Space: 12.5 MB (12.33%) - LeetHub

---
+++ 2427-number-of-common-factors/2427-number-of-common-factors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def commonFactors(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        count=0
+        limit=min(a,b)
+        for i in range(1,limit+1):
+            if a%i==0 and b%i==0:
+                count+=1
+        return count
