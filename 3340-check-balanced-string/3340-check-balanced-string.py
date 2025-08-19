616283f4c9b0006b62c6895f97ef8442d2f080d6
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-08-18 22:14:36 -0400

    Time: 0 ms (100%), Space: 12.5 MB (13.14%) - LeetHub

---
+++ 3340-check-balanced-string/3340-check-balanced-string.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isBalanced(self, num):
+        """
+        :type num: str
+        :rtype: bool
+        """
+        even_sum,odd_sum=0,0
+        for i in range(len(num)):
+            if i%2==0:
+                even_sum+=int(num[i])
+            else:
+                odd_sum+=int(num[i])
+        return even_sum==odd_sum
