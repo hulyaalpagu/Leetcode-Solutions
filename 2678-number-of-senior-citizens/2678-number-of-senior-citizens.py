8154218bf52cf975d7b871693da3a7dc18bfe096
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-08-30 21:50:57 -0400

    Time: 0 ms (100%), Space: 12.3 MB (85.86%) - LeetHub

---
+++ 2678-number-of-senior-citizens/2678-number-of-senior-citizens.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def countSeniors(self, details):
+        """
+        :type details: List[str]
+        :rtype: int
+        """
+        count=0
+        for detail in details:
+            age=int(detail[11:13])
+            if age>60:
+                count+=1
+        return count
