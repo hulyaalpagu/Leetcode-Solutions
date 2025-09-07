cd16fbf6c8cf8519dde12e9047a671ccf9386881
Hulya Alpagu <134128982+hulyaalpagu@users.noreply.github.com>
2025-09-07 01:01:44 -0400

    Time: 10 ms (91%), Space: 12.5 MB (49.33%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def interpret(self, command):
+        """
+        :type command: str
+        :rtype: str
+        """
+        result=""
+        i=0
+        while i<len(command):
+            if command[i]=="G":
+                result+="G"
+                i+=1
+            elif command[i:i+2]=="()":
+                result+="o"
+                i+=2
+            elif command[i:i+4]=="(al)":
+                result+="al"
+                i+=4
+        return result
+
